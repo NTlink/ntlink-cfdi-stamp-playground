af6aa85d02f661f4608858b86c82c5df915048ef
havelino <hugo.avelino@ntlink.com.mx>
2025-02-25 16:49:15 -0500

    Enable reading XMl files
    Signed-off-by: havelino <hugo.avelino@ntlink.com.mx>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ntlink-cfdi-stamp-playground';
+
+    @ViewChild('fileInput') 
+    public fileInput!: ElementRef;
+    public loading: boolean = false;
+    public xml: string | undefined;
+    
+
+
+    onFileChange(event: Event) {
+
+      const inputE = (event.target as HTMLInputElement);
+      const file = inputE.files ? inputE.files[0] : undefined;
+      if (file) {
+      const reader = new FileReader();
+      reader.readAsText(file);
+        reader.onload = () => {
+          this.xml = reader?.result?.toString();
+        };
+        reader.onerror = () => {
+            console.error('Error cargando el archivo');
+            //this.resetFileInput();
+        };
+    }
+  }
+ 
 }
